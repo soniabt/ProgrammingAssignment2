85754f2fe14d2c345a4277cb01161ac102237977
soniabt <sonia_bauer@hotmail.com>
2015-03-22 09:15:06 -0400

    made changes to cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## create a matrix then cache the inverse of the matrix with two functions: 
+## makeCacheMatrix
+## cacheSolve
 
-## Write a short comment describing this function
+
+## makeCacheMatrix 
+## Create a square matrix - a list containing a function to 
+##      1. set the value of the matrix
+##      2. get the value of the matrix
+##      3. set the value of the inverse
+##      4. get the value of hte inverse
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        # check to see if matrix is square:
+        if (nrow(x)!=ncol(x)) {
+                print("matrix is not square - exit function")
+                return()
+        }
+        invm <<- NULL
+        set <- function(y) {
+                x <<- y
+                invm <- matrix(rep(NA,(nrow(x))*(ncol(x))),nrow(x),ncol(x))
+        }
+        get <- function() x
+        setinv <- function(solve) invm <<- solve
+        getinv <- function() invm
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)    
 }
 
+## cacheSolve
+## Calculate the inverse of the matrix created in makeCacheMatrix - 
+## function first checks to see if the inverse has already been calculated
+## If so, cacheSolve gets the inverse from a cache and skips the computation.
+## Otherwise, it calculates the inverse of the matrix and sets the inverse in 
+## the case via   setinv   function.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x=matrix(), ...) {
         ## Return a matrix that is the inverse of 'x'
+                invm <- x$getinv()
+                if(!is.null(invm)) {
+                        message("getting cached data")
+                        return(invm)
+                }
+                data <- x$get()
+                invm <- solve(data, ...)
+                x$setinv(invm)
+        invm
 }
